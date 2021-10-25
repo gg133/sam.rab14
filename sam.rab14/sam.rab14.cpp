e3b9e980935afda5ba688fae02bcd37a455bf234
gg133 <133chess@gmail.com>
2021-10-25 15:05:09 +0300

    qw

--- sam.rab14/sam.rab14.cpp
+++ sam.rab14/sam.rab14.cpp
@@ -3,69 +3,69 @@ using namespace std;
 
 int main() {
 	setlocale(0, "");
-	const int a1 = 10;
-	const int a2 = 10;
-	const int a3 = 10;
-	const int a4 = 10;
-	const int a5 = 10;
-	const int a6 = 10;
-	const int a7 = 10;
-	const int a8 = 10;
-	int arr1[a1];
-	double arr2[a2];
-	float arr3[a3];
-	char arr4[a4];
-	bool arr5[a5];
-	long arr6[a6];
-	short arr7[a7];
-	string arr8[a8];
+	const int A1 = 10;
+	const int A2 = 10;
+	const int A3 = 10;
+	const int A4 = 10;
+	const int A5 = 10;
+	const int A6 = 10;
+	const int A7 = 10;
+	const int A8 = 10;
+	int arr1[A1];
+	double arr2[A2];
+	float arr3[A3];
+	char arr4[A4];
+	bool arr5[A5];
+	long arr6[A6];
+	short arr7[A7];
+	string arr8[A8];
 
-	cout << "Первый массив : \n";
-	for (int i = 0; i < a1; i++) {
+	cout << "Int массив : \n";
+	for (int i = 0; i < A1; i++) {
 		arr1[i] = i + 10;
 		cout << "[" << i << "] arr: " << arr1[i] << "\n";
 	}
 	cout << endl;
-	cout << "Второй массив : \n";
-	for (int i = 0; i < a2; i++) {
+	cout << "Double массив : \n";
+	for (int i = 0; i < A2; i++) {
 		arr2[i] = i + 0.55;
 		cout << "[" << i << "] arr: " << arr2[i] << "\n";
 	}
 	cout << endl;
-	cout << "Третий массив : \n";
+	cout << "Float массив : \n";
 
-	for (int i = 0; i < a3; i++) {
+	for (int i = 0; i < A3; i++) {
 		arr3[i] = i + 0.5;
 		cout << "[" << i << "] arr: " << arr3[i] << "\n";
 	}
 	cout << endl;
-	cout << "Четвертый массив : \n";
+	cout << "Char массив : \n";
 
-	for (int i = 0; i < a4; i++) {
+	for (int i = 0; i < A4; i++) {
 		arr4[i] = i + '!';
 		cout << "[" << i << "] arr: " << arr4[i] << "\n";
 	}
 	cout << endl;
-	cout << "Пятый массив : \n";
-	for (int i = 0; i < a5; i++) {
+	cout << " Bool массив : \n";
+	for (int i = 0; i < A5; i++) {
 		arr5[i] = false;
 		cout << "[" << i << "] arr: " << arr5[i] << "\n";
 	}
 	cout << endl;
-	cout << "Шестой массив : \n";
-	for (int i = 0; i < a6; i++) {
+	cout << "Long массив : \n";
+	for (int i = 0; i < A6; i++) {
 		arr6[i] = i + 100;
 		cout << "[" << i << "] arr: " << arr6[i] << "\n";
 	}
 	cout << endl;
-	cout << "Седьмой массив : \n";
-	for (int i = 0; i < a7; i++) {
+	cout << "Short массив : \n";
+	for (int i = 0; i < A7; i++) {
 		arr7[i] = i;
 		cout << "[" << i << "] arr: " << arr7[i] << "\n";
 	}
 	cout << endl;
-	cout << "Восьмой массив : \n";
-	for (int i =0; i < a8; i++) {
+	cout << "String массив : \n";
+	for (int i =0; i < A8; i++) {
 		arr8[i] = "qwertyuiop" + i;
 		cout << "[" << i << "] arr: " << arr8[i] << "\n";
 	}
